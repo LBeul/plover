31e9efe67b3afbc672c46b0e5b340dc36d3608b7
LBeul <l.beul@outlook.de>
2019-08-31 03:26:27 +0200

    Destructured props

--- src/components/CoverPreview.jsx
+++ src/components/CoverPreview.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "../styles/CoverPreview.css";
 
-function CoverPreview(props) {
+function CoverPreview({ image, artistLink, artist }) {
   return (
     <div className="cover-prev">
       {/* Picture result based on searched topic */}
-      <img src={props.image} alt="Your Cover!" />
+      <img src={image} alt="Your Cover!" />
       {/* Reference according to unsplash API guidelines */}
       <p className="credits">
-        Picture by <a href={props.artistLink}>{props.artist}</a> from{" "}
+        Picture by <a href={artistLink}>{artist}</a> from{" "}
         <a href="https://unsplash.com/">Unsplash</a>
       </p>
       <button>Download!</button>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 import "../styles/InputBox.css";
 
-function InputBox(props) {
+function InputBox({ handleSubmit, handleTopicInput, handleCaptionInput }) {
   return (
-    <form className="input-box" onSubmit={props.handleSubmit}>
+    <form className="input-box" onSubmit={handleSubmit}>
       {/* Topic input for image search */}
-      <input
-        type="text"
-        placeholder="topic"
-        onChange={props.handleTopicInput}
-      />
+      <input type="text" placeholder="topic" onChange={handleTopicInput} />
       <br />
       {/* Caption aka name of playlist */}
-      <input
-        type="text"
-        placeholder="caption"
-        onChange={props.handleCaptionInput}
-      />
+      <input type="text" placeholder="caption" onChange={handleCaptionInput} />
       <br />
       {/* Button that triggers the handleClick function given as props */}
       <input id="button" type="submit" value="Generate!" />
